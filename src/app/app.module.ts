391303c9c46adf8c2b98faa85f3f69417acdf087
faustino01 <20181165@uthh.edu.mx>
2023-11-28 23:55:35 -0600

    estilo menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListGeneralComponent } from './list-general/list-general.component';
 import { MaterialModule } from './material/material/material.module';
+import { MenuComponent } from './menu/menu.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListGeneralComponent
+    ListGeneralComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
